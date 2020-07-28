5876a1e58328117e8ed8d2f993d912097086b580
thflthdi <thflthdi@gmail.com>
2020-07-28 21:46:49 +0900

    add: 챕터8 - API 서버와 REST, JSON응답뷰 만들기, JSON 직렬화, APIView를 활용한 뷰

---
+++ chap8/drf_project_project/instagram/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('post', views.PostViewSet)
+#router.urls
+
+
+urlpatterns = [
+    path('public/', views.public_post_list, name='public'),
+    path('', include(router.urls)),
+]
