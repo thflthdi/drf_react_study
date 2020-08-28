28508b009508f7376ec242d3d67b1ce0f887ecde
thflthdi <thflthdi@gmail.com>
2020-08-28 14:55:50 +0000

    Updates

---
+++ precache-manifest.7ab60cde785103bbbd873d17f474e726.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5467ef99a4777efe42aa45f729ddb9fb",
+    "url": "/drf_react_study/index.html"
+  },
+  {
+    "revision": "13d8518a05e633a352a7",
+    "url": "/drf_react_study/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "5668edc158c933ed7c78",
+    "url": "/drf_react_study/static/css/main.1da18e75.chunk.css"
+  },
+  {
+    "revision": "13d8518a05e633a352a7",
+    "url": "/drf_react_study/static/js/2.da56db6a.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/drf_react_study/static/js/2.da56db6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5668edc158c933ed7c78",
+    "url": "/drf_react_study/static/js/main.63376996.chunk.js"
+  },
+  {
+    "revision": "7d6f245978910a7a5eb6",
+    "url": "/drf_react_study/static/js/runtime-main.a1be58a4.js"
+  }
+]);
